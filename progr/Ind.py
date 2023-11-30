9221566cc4d4a62c79e35539aa74b52d29dde9da
Daniletskiy-D <slimedodoser@gmail.com>
2023-11-30 18:58:29 +0300

    правки

--- progr/Ind.py
+++ progr/Ind.py
@@ -4,7 +4,7 @@
 if __name__ == "__main__":
     A = tuple(map(int, input().split()))
 
-    for i in range(len(A) - 2):
+    for i, value in enumerate(A[:-2]):
         if A[i + 1] > A[i] and A[i + 1] > A[i + 2]:
             print("Номера элементов первой тройки:", i + 1, i + 2, i + 3)
             break
